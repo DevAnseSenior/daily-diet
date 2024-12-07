d29f2b6f8cbb5c8a2593e9d8da88628128286be9
Anderson Coelho <anderson.coelho@indt.org.br>
2024-12-07 19:36:27 -0400

    create a test on Create endpoint and categorize tests

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -1,7 +1,26 @@
-import { expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, it } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('user must create a new meal', () => {
-  const responseStatesCode = 201
+describe('Meals routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-  expect(responseStatesCode).toEqual(201)
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('user must create a new meal', async () => {
+    await request(app.server)
+      .post('/meals')
+      .send({
+        name: 'new meal',
+        description: 'new meal description',
+        data: '2024-12-02',
+        hour: '09:00:23',
+        diet: 'yes',
+      })
+      .expect(201)
+  })
 })
